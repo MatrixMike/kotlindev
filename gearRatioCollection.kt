f0cf5e0eed02640256ee64ea4c1ab61dba013194
Mike H <matroxmike@icloud.com>
2018-06-26 13:30:38 +1000

    force sort

--- gearRatioCollection.kt
+++ gearRatioCollection.kt
@@ -2,9 +2,17 @@
  * We declare a package-level function main which returns Unit and takes
  * an Array of strings as a parameter. Note that semicolons are optional.
  */
+/*
+26.06.2018 12:21:10
+my aim is to display (create) a sorted list of the bike gear ratios produced by
+dividing the front gear size by the rear gear size
+A function that took two collections of front and rear gear sizes and returned
+a collection of the sorted ratios would be useful. Further I would like to retain 
+the cause of the ratio i.e. which front/rear gear combination caused that ratio.
+*/
 
 fun main(args: Array<String>) {
-        val frontGears = listOf(22.0,32.0,42.0)
+        val frontGears = listOf(42.0, 32.0, 22.0)   // deliberately not sorted
         val rearGears = listOf(14.0,16.0,20.0,30.0,48.0)
         val items = setOf("apple", "banana", "kiwi")
     when {
@@ -27,8 +35,9 @@ fun main(args: Array<String>) {
     }   
     val numbers: MutableList<Int> = mutableListOf(1, 2, 3)
 val readOnlyView: List<Int> = numbers
-println(numbers)        // prints "[1, 2, 3]"
-numbers.add(4)
+println(numbers)        // prints "[4, 2, 3]"
+numbers.add(2)   // .sortedBy{it}.forEach {println(it) }
+
 println(readOnlyView)   // prints "[1, 2, 3, 4]"
 //readOnlyView.clear()    // -> does not compile
 
