d417137ddc819cdd71b6f88cb0e1af3693ebf343
Mike H <matroxmike@icloud.com>
2018-06-27 12:20:41 +1000

    refined

---
+++ gearRatioCollection2.kt
@@ -0,0 +1,36 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+fun main(args: Array<String>) {
+        val frontGears = listOf(22.0,32.0,42.0)
+        val rearGears = listOf(14,16,20,30,48)
+        val items = setOf("apple", "banana", "kiwi")
+    when {
+        "orange" in items -> println("juicy")
+        "apple" in items -> println("apple is fine too")
+    }
+
+    println("Hello, world!")
+    frontGears
+    .sortedBy{it}
+    .forEach { println(it/2.0) }
+
+    val numbers: MutableList<Double> = mutableListOf()
+    for (f in frontGears){
+        for (r in rearGears)
+        numbers.add(f/r)
+    }
+//    println(numbers)  // prints the whole collection
+//    numbers.sortedBy{it}.forEach{println(it)}  // prints unformatted
+//    val formatted = String.format("%.2f   %.2f %.2f", f/r, f, r ) ;
+//        println(formatted) 
+    numbers
+    .sortedBy{it}
+    .forEach{ println("%6.3f  ".format(it))}
+}
+//    numbers.add(2.3)
+//    numbers.add(2.0)    
+//    println(numbers)
+
--- kotlinTips.md
+++ kotlinTips.md
@@ -1,10 +1,12 @@
 <ol>
 <li>kotlin - get it from <a href="https://github.com/JetBrains/kotlin/releases/tag/v1.2.0"> here </a>for command line
-</li><li>try it <a href= https://try.kotlinlang.org/#/Kotlin%20Koans/Introduction/Java%20to%20Kotlin%20conversion/Task.kt">online </a> could login with say, github
+</li><li>try it <a href="https://try.kotlinlang.org/#/Kotlin%20Koans/Introduction/Java%20to%20Kotlin%20conversion/Task.kt">online </a> could login with say, github
 </li><li>
 typing standards
 </li><li>
 some conversion from Java examples
+</li><li>
+<a href="http://kotlinlang.org/docs/resources.html">other resources</a>
 </li>
 </ol>
 
