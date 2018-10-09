cf05e418bcec65fb432a066e29066d8d1857dd6a
Mike <matroxmike@icloud.com>
2018-10-10 00:28:21 +1100

    checked script

--- kotlin-javaScript.sh
+++ kotlin-javaScript.sh
@@ -1,4 +1,4 @@
-#!
+#!/bin/bash
 ~/kotlinc/bin/kotlinc-js -output sample-library.js -meta-info                        library.kt
 ~/kotlinc/bin/kotlinc-js -output binom.js          -libraries sample-library.meta.js binom.kt
 ls -lctrh
