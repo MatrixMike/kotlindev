8ae208f2d380ad0437cf7fe270a92076d72494c2
Mike H <matroxmike@icloud.com>
2019-07-18 00:28:35 +1000

    when with enum

--- ch02Ex33.kt
+++ ch02Ex33.kt
@@ -7,18 +7,19 @@ package ch02.ex3_3_UsingWhenWithArbitraryObjects
 // printing the word in the enum list
 
 enum class Color {
-    RED, ORANGE, YELLOW, GREEN1, BLUE, INDIGO, VIOLET
+    RED, ORANGE, YELLOW, GREEN_1, BLUE, INDIGO, VIOLET
 }
 fun mix(c1: Color, c2: Color) =
         when (setOf(c1, c2)) {
             setOf(Color.RED,    Color.YELLOW) -> Color.ORANGE
-            setOf(Color.YELLOW, Color.BLUE)   -> Color.GREEN1
+            setOf(Color.YELLOW, Color.BLUE)   -> Color.GREEN_1
             setOf(Color.BLUE,   Color.VIOLET) -> Color.INDIGO
             else -> throw Exception("Dirty color")
         }
 
 fun main(args: Array<String>) {
     println(mix(Color.BLUE, Color.YELLOW))
+    println(mix(Color.YELLOW, Color.BLUE))
     println(mix(Color.RED,  Color.YELLOW))
 //    println(mix(Color.BLUE, Color.BLUE))   // uncomment this for Exception
 }
