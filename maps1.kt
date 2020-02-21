ad8c201b52e3883abe24e6f92d00ecf02f5bfd20
Mike <matroxmike@icloud.com>
2020-02-21 13:47:09 +1100

    reify tests 2

--- maps1.kt
+++ maps1.kt
@@ -1,9 +1,11 @@
 //import java.util.*
 
 fun main(args: Array<String>) {
-    val readWriteMap: HashMap< String, Int > = hashMapOf("foo" to 1, "bar" to 2, "xtc" to 4)
+    val readWriteMap: HashMap< String, Int > = hashMapOf("foo" to 1, "bar" to 2, "xtc" to 4, args[1] to 6)
     println( readWriteMap )  // prints "1"
     //     println(readWriteMap["foo"])  // prints "1"
     val snapshot: Map<String, Int> = HashMap(readWriteMap)
     println(snapshot)
+    println(args)
+    println(args[1])
 }
