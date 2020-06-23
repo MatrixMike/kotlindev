fd4a57907dc1b4c4c912b69e731fbb6f27885085
Mike <matroxmike@icloud.com>
2020-06-23 20:37:54 +1000

    main module

--- gearRatioCollection.kt
+++ gearRatioCollection.kt
@@ -43,17 +43,4 @@ val strings = hashSetOf("a", "b", "c", "c")
 assert(strings.size == 3)
 }
 
-fun gearsOut () {
-    var formatted ="%"
-        val frontGears = listOf(42.0, 32.0, 22.0)   // deliberately not sorted
-        val rearGears = listOf(26.0, 23.0, 21.0, 19.0, 17.0, 15.0, 13.0, 12.0)  // number of teeth on gear is an integer
-    for (f in frontGears){
-    for (r in rearGears){
-         formatted = formatted + String.format("*%.2f   %.2f   %.2f   \n", f/r, f, r ) ;
 
-		}
-	println("next f")
-    } 
-    println(formatted)
-
-}
