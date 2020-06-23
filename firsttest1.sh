bb72ff0da0906b6d4f5db8490b9a27fa8d8ecc4e
Mike J H <matrixmike@icloud.com>
2020-06-23 15:14:58 +1000

    mod to script

--- firsttest1.sh
+++ firsttest1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-~/kotlinc/bin/kotlinc hello.kt -include-runtime -d hello.jar -verbose
+## ~/kotlinc/bin/
+kotlinc hello.kt -include-runtime -d hello.jar -verbose
 java -jar hello.jar  4 5 6
 
--- firsttest2.sh
+++ firsttest2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-~/kotlinc/bin/kotlinc bezier1.kt -include-runtime -d bezier1.jar
+## ~/kotlinc/bin/
+kotlinc bezier1.kt -include-runtime -d bezier1.jar
 java -jar bezier1.jar  4 5 6
 xviewer quadratic_bezier.jpg &
 
