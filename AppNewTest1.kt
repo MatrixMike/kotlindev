41c93e60f706bf287df6b659f4331cdb780b2ce0
mikeh (ASUS) <matroxmike@icloud.com>
2020-08-06 14:21:46 +1000

    boxed experiments

---
+++ AppNewTest1.kt
@@ -0,0 +1,20 @@
+//private lateinit var latitudeText: TextView
+//latitudeText  = findViewById(R.id.latitude_text)
+
+//latitudeText.text = resources
+//	.getString(R.string.latitude_label, location?.latitude)    
+
+
+fun main(args: Array<String>) { 
+val a: Int = 100
+val boxedA: Int? = a
+val anotherBoxedA: Int? = a
+
+val b: Int = 10000
+val boxedB: Int? = b
+val anotherBoxedB: Int? = b 
+
+val i: Int = b.toInt() // OK: explicitly widened
+print(i)  
+    }
+    
