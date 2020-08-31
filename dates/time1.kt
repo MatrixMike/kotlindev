c79cd34d65dfeb4d6e533df27e70d795be2f6c6f
mikeh (ASUS) <matroxmike@icloud.com>
2020-08-31 15:44:25 +1000

    zoned time

--- dates/time1.kt
+++ dates/time1.kt
@@ -6,6 +6,8 @@ import java.time.LocalDate;  // Import the LocalDateTime class
 import java.time.LocalDateTime;  // Import the LocalDateTime class
 import java.time.format.DateTimeFormatter;  // Import the DateTimeFormatter class
 import java.time.ZonedDateTime;
+//import java.time.format.ZonedDateTime;
+
 // https://www.ict.social/kotlin/oop/date-and-time-in-kotlin-parsing-and-comparing
 
 fun main(args: Array<String>) {
@@ -13,8 +15,13 @@ fun main(args: Array<String>) {
 	val dateTime = LocalDateTime.parse("2016-12-08T10:20:30")
 	val date = LocalDate.parse("2016-12-08")
 	val time = LocalTime.parse("10:20:30")
-
+//	val zonedTime = LocalDateTime.parse("2016-12-08T10:20:30")
 	println(dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)))
 	println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)))
 	println(time.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)))
+//	DateTimeFormatter formatter6 = DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a z");
+
+//    println( formatter6.format(ZonedDateTime.now()));
+ //   println(dateTime.format(ZonedDateTime.now()))
+ println("Local ZonedDateTime = ${ZonedDateTime.now()}")
 }
