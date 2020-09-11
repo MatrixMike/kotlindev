20123a401673f38e561e1f2eb5969edc7d2e0ccd
mikeh (ASUS) <matroxmike@icloud.com>
2020-09-11 19:54:35 +1000

    happy examples

---
+++ classExample1.kt
@@ -0,0 +1,26 @@
+/**
+* Created by www.tutorialkart.com
+* main function in kotlin example program
+https://www.tutorialkart.com/kotlin/main-function-in-kotlin/
+*/
+ 
+fun main() { // was fun main(args: Array)
+    val user1 = User(name="Yogi", age=27)
+    printUser(user1)
+    
+        var daysOfWeek = listOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+    for((index,day) in daysOfWeek.withIndex()){
+        println("$index :$day")
+    } 
+    
+    var nums = listOf(25, 54, 68, 72)
+    for((index,num) in nums.withIndex()){
+        println(index.toString()+" - "+num)
+    } 
+}
+ 
+fun printUser(user: User){
+    println(user)
+}
+ 
+data class User(val name: String, val age: Int); 
